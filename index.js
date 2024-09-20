d21171d2e7fa5c65cfca6702865bbc63b54b36e2
miawpuspita <puspitasari22232@gmail.com>
2024-09-20 11:21:20 +0700

    baru

--- index.js
+++ index.js
@@ -1,38 +1,36 @@
-// inisialisasi model http
+// inisialisasi modul http
 const http = require('http');
 
 // buat server
-const server = http.createServer((req, res)=>
+const server = http.createServer((req, res)=> 
 {
-    // res.writeHead(200, { 'content-Type' : 'text/html'});
-    // res.write('selamat pagi');
+   // res.writeHead(200, {'Content-Type': 'text/html'});
+   // res.write('Selamat pagi');
 
-    res.writeHead(200, { 'content-Type' : 'application/json'});
+    res.writeHead(200, {'Content-Type': 'application/json'});
 
-    if(req.url ==='/dosen'){
-        message ="List data dosen";
-        data = ['ahamad','hafiz','widi'];
-    } else if(req.url === '/mahasiswa') {
-        message ="List data mahasiswa";
-        data = ['puspita','angel','nabila'];
+    if(req.url === '/dosen'){
+        message ='List Data Dosen';
+        data =['Ahmad','Hafiz','Widi'];
+    }else if(req.url === '/mahasiswa'){
+        message='List Data Mahasiswa';
+        data=['Kenny','Rizky','Akbar'];
     }else{
-        message ="Tidak ditemukan";
-        data = [];
-
-
+        message ='Tidak ditemukan';
+        data= [];
     }
     res.write(JSON.stringify({
-        'message': message,
-        'data'   : data,
+        'message' : message,
+        'data' : data,
         'status' : 'success'
     }));
     res.end();
-} );
+});
 
 const port = 3000;
-const host = 'localhost';
+const host ='localhost';
 
-server.listen(port, host, () => {
-    console.log(`server berjalan di http://${host}:${port}`);
-
+server.listen(port, host, ()=> {
+    console.log(`Server berjalan di http://${host}:${port}`);
 });
+
