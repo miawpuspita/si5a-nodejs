40df4a26aeec91c22042b2273458e7145c7736cd
miawpuspita <puspitasari22232@gmail.com>
2024-09-20 11:11:18 +0700

    ..

--- index.js
+++ index.js
@@ -8,6 +8,19 @@ const server = http.createServer((req, res)=>
     // res.write('selamat pagi');
 
     res.writeHead(200, { 'content-Type' : 'application/json'});
+
+    if(req.url ==='/dosen'){
+        messeage ="List data dosen";
+        data = ['ahamad','hafiz','widi'];
+    } else if(req.url === '/mahasiswa') {
+        messeage ="List data mahasiswa";
+        data = ['puspita','angel','nabila'];
+    }else{
+        messeage ="Tidak ditemukan";
+        data = [];
+
+
+    }
     res.write(JSON.stringify({
         'message': 'selamat pagi',
         'status' : 'success'
